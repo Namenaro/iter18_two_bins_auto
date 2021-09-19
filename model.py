9622aaed490f4c035d9c64d3aec8a40e21fba912
neuro <nanenaro@gmail.com>
2021-09-19 17:43:30 +0300

    rthgf

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
+from utils import *
+
 class Model:
-    def __init(self, train_sample, test_sample):
+    def __init(self, train_sample):
         self.train_sample = train_sample
-        self.test_sample = test_sample
 
     def sample(self, sample_size, nbins):
-        probs, bins = get_hist(train_sample, nbins)
+        probs, bins = get_hist(self.train_sample, nbins)
+        return sample_from_hist(probs, bins, sample_size)
